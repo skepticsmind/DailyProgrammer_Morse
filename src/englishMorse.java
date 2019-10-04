e16f832e4959034149c7b0efc96f43b857b0588b
skepticsmind <skepticsmind@gmail.com>
2019-10-03 21:41:25 -0400

    Changed english enum from string to char

--- src/englishMorse.java
+++ src/englishMorse.java
@@ -1,36 +1,36 @@
 public enum englishMorse {
     //Enumerator that stores the english alphabet letter and the morse code that is attributed to each letter.
-    A("a", ".-"),
-    B("b", "-..."),
-    C("C", "-.-."),
-    D("d", "-.."),
-    E("e", "."),
-    F("f", "..-."),
-    G("g", "--."),
-    H("h", "...."),
-    I("i", ".."),
-    J("j", ".---"),
-    K("k", "-.-"),
-    L("l", ".-.."),
-    M("m", "--"),
-    N("n", "-."),
-    O("o", "---"),
-    P("p", ".--."),
-    Q("q", "--.-"),
-    R("r", ".-."),
-    S("s", "..."),
-    T("t", "-"),
-    U("u", "..-"),
-    V("v", "...-"),
-    W("w", ".--"),
-    X("x", "-..-"),
-    Y("y", "-.--"),
-    Z("z", "--..");
+    A('a', ".-"),
+    B('b', "-..."),
+    C('C', "-.-."),
+    D('d', "-.."),
+    E('e', "."),
+    F('f', "..-."),
+    G('g', "--."),
+    H('h', "...."),
+    I('i', ".."),
+    J('j', ".---"),
+    K('k', "-.-"),
+    L('l', ".-.."),
+    M('m', "--"),
+    N('n', "-."),
+    O('o', "---"),
+    P('p', ".--."),
+    Q('q', "--.-"),
+    R('r', ".-."),
+    S('s', "..."),
+    T('t', "-"),
+    U('u', "..-"),
+    V('v', "...-"),
+    W('w', ".--"),
+    X('x', "-..-"),
+    Y('y', "-.--"),
+    Z('z', "--..");
 
-    private String english;
+    private char english;
     private String morse;
 
-    englishMorse(String e, String m){
+    englishMorse(char e, String m){
         this.english = e;
         this.morse = m;
     }
@@ -40,7 +40,7 @@ public enum englishMorse {
         return this.morse;
     }
 
-    public String toEnglish(){
+    public char toEnglish(){
         return this.english;
     }
 }
